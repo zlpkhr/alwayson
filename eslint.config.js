cfb0c25ded7e642ef5f1678897c32e036f2344ec
Azamat Zulpykhar <zlpkhr@icloud.com>
2025-06-28 01:41:42 +0200

    Ignore public folder

--- eslint.config.js
+++ eslint.config.js
@@ -7,7 +7,7 @@ import globals from "globals";
 import tseslint from "typescript-eslint";
 
 export default tseslint.config([
-  globalIgnores(["dist"]),
+  globalIgnores(["dist", "public"]),
   {
     files: ["**/*.{ts,tsx}"],
     extends: [
