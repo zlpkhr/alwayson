895bd9f721d332b725655d66650bdd7d1138e2bf
Azamat Zulpykhar <zlpkhr@icloud.com>
2025-06-28 01:33:08 +0200

    Setup prettier

--- eslint.config.js
+++ eslint.config.js
@@ -4,6 +4,7 @@ import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
 import tseslint from "typescript-eslint";
 import { globalIgnores } from "eslint/config";
+import eslintConfigPrettier from "eslint-config-prettier/flat";
 
 export default tseslint.config([
   globalIgnores(["dist"]),
@@ -20,4 +21,5 @@ export default tseslint.config([
       globals: globals.browser,
     },
   },
+  eslintConfigPrettier,
 ]);
