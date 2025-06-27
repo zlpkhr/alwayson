8fb41bb59d5c7e0427c0dd08589c8829079c5a38
Azamat Zulpykhar <zlpkhr@icloud.com>
2025-06-28 01:53:10 +0200

    Setup tailwindcss

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,9 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import "./main.css";
 
-createRoot(document.getElementById("root")!).render(<StrictMode />);
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <h1 className="text-3xl font-bold underline">Hello world!</h1>
+  </StrictMode>,
+);
