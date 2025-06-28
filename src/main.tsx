7e388200cd517aa023d992f5f7d17880f4c28eee
Azamat Zulpykhar <zlpkhr@icloud.com>
2025-06-29 01:26:28 +0200

    Install react query devtools

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,5 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./app";
@@ -11,6 +12,7 @@ createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
       <Messenger />
+      <ReactQueryDevtools />
     </QueryClientProvider>
   </StrictMode>,
 );
