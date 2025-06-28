87b1bccd73ee535e8abd17137a4b67dbe432e77f
Azamat Zulpykhar <zlpkhr@icloud.com>
2025-06-29 01:24:30 +0200

    Install react query

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,16 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./app";
 import "./main.css";
 import { Messenger } from "./messenger";
 
+const queryClient = new QueryClient();
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <Messenger />
+    <QueryClientProvider client={queryClient}>
+      <Messenger />
+    </QueryClientProvider>
   </StrictMode>,
 );
